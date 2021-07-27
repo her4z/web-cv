b0a4cf41c3a207772e37fde6556b14e6454057df
sebasheras <herassebastian@gmail.com>
2021-07-27 03:05:34 -0300

    rickroll opens on another tab

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ class App extends React.Component {
         <header className="App-header">
           <h1>Sorry, this isn't finished yet :/</h1>
           <h3>"Lo siento, esto aún no está terminado."</h3>
-          <button> <a href="https://youtu.be/dQw4w9WgXcQ">Click me for a sneak peek!</a> </button>
+          <button> <a target="_blank" href="https://youtu.be/dQw4w9WgXcQ">Click me for a sneak peek!</a> </button>
         </header>
       </div>
     );
