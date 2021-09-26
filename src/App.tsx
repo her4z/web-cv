090cc1f4f21412e963492ced21c6fb7b2c477aa3
sebasheras <herassebastian@gmail.com>
2021-09-26 04:50:01 -0300

    roboto font and material icons imported on app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ class App extends React.Component {
   render(){
     return (
       <div className="App">
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
           <Home></Home>
       </div>
     );
