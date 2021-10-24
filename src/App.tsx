0f2760e0b05b9ce75759089c784775ca152ed3e6
sebasheras <herassebastian@gmail.com>
2021-10-24 00:03:02 -0300

    theme switch added, also changes on root height

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home/Home';
 
+
+ 
 class App extends React.Component {
+  
   render(){
+
     return (
       <div className="App">
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,12 @@
 import './Home.css';
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import { createTheme, Paper, ThemeProvider } from '@material-ui/core';
+import Switch from '@material-ui/core/Switch';
+import NightStayIcon from '@material-ui/icons/NightsStay';
+import WbSunnyIcon from '@material-ui/icons/WbSunny';
+
+
 
 
 
@@ -9,6 +15,7 @@ export default class Home extends React.Component<{}, any> {
     constructor(props:any) {
         super(props);
         this.state = {
+            checked: true
         };
     }
 
@@ -24,22 +31,34 @@ export default class Home extends React.Component<{}, any> {
     }
 
     render() {
+        const theme = createTheme({
+            palette:{
+              mode: this.state.checked === true ? 'dark' : 'light'
+            }
+          });
         return (
-            <div className="container" style={{width: '100%', height: '100%'}}>
-                <Grid container spacing={2}>
-                    <Grid item xs={12} className="welcome-text">
-                        <div className={`welcome-text-first animate__animated ${this.state.firstAnimation}`}>
-                            <text>Hi,</text>
-                        </div>
-                    </Grid>
-                    <Grid item xs={12} className="welcome-text">
-                        <div className={`welcome-text-second animate__animated ${this.state.secondAnimation}`}>
-                            <text >I'm Sebastian</text>
-                        </div>
-                    </Grid>
+            <ThemeProvider theme={theme}>
+                <Paper style={{height: '100%', width: '100%'}}>
+                    <div className="container">
+                        <Grid container spacing={2}>
+                            <Grid item xs={3}>
+                                <Switch checked={this.state.checked} onChange={()=>this.setState({checked: this.state.checked === true ? false : true})} checkedIcon={<NightStayIcon htmlColor="white"></NightStayIcon>} icon={<WbSunnyIcon htmlColor="yellow"></WbSunnyIcon>}></Switch>
+                            </Grid>
+                            <Grid item xs={12} className="welcome-text">
+                                <div className={`welcome-text-first animate__animated ${this.state.firstAnimation}`}>
+                                    <text>Hi,</text>
+                                </div>
+                            </Grid>
+                            <Grid item xs={12} className="welcome-text">
+                                <div className={`welcome-text-second animate__animated ${this.state.secondAnimation}`}>
+                                    <text >I'm Sebastian</text>
+                                </div>
+                            </Grid>
 
-                </Grid>
-            </div>
+                        </Grid>
+                    </div>
+                </Paper>
+          </ThemeProvider>
         )
     }
 }
